f06decf2e006cad621a310a6a7768fb94f5a209b
jkress <jonathank1993@gmail.com>
2023-06-18 11:16:32 -0500

    Add styles and scroll bar

--- reactpractice/src/App.js
+++ reactpractice/src/App.js
@@ -1,33 +1,50 @@
 import React from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
+import Scroll from './Scroll';
+import './App.css';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value });
+  componentDidMount() {
+    // Fetch placeholder set of robots (users) and assign to state
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }));
   }
 
   render() {
+    // Use searchfield to filter out list of robots to display
     const filteredRobots = this.state.robots.filter((robot) => {
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
     });
 
+    // Display loading if robots file hasn't been loaded yet
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>
+    }
+
     return (
       <div className='tc'>
-        <h1>RoboFriends</h1>
+        <h1 className='f1'>RoboFriends</h1>
         <SearchBox searchfield={this.state.searchfield} searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} searchfield={this.state.searchfield} />
+        <Scroll>
+          <CardList robots={filteredRobots} searchfield={this.state.searchfield} />
+        </Scroll>
       </div>
     );
+  }
+
+  // Event that runs anytime the robot name search field is changed. Updates searchfield state.
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
   }
 }
 
--- reactpractice/src/SearchBox.js
+++ reactpractice/src/SearchBox.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class SearchBox extends React.Component {
   render() {
-    const {searchfield, searchChange} = this.props;
+    const {searchChange} = this.props;
     return (
       <div className='pa2'>
         <input className='pa3 ba b--green bg-lightest-blue' 
